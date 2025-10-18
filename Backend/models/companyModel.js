86f9c864fa2e505b9d6d967feeb9c14c2dc0721b
tiwari786 <vipultiwari479@gmail.com>
2025-10-18 16:16:00 +0530

    created models(user,job,company,application)

---
+++ Backend/models/companyModel.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const companySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String
+    },
+    website: {
+        type: String
+    },
+    location: {
+        type: String
+    },
+    logo: {
+        type: String // URL or path to the company logo
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId, ref: 'User',
+        required: true
+    }
+
+}, { timestamps: true });
+
+const Company = mongoose.model("Company", companySchema);
+export default Company;
---
+++ Backend/models/userModel.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    fullname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    contact: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ['student', 'recruiter'],
+        required: true
+    },
+    profile: {
+        bio: { type: String }, // Added bio field
+        skills: [{ type: String }], // Added skills field
+        resume: { type: String }, // URL or path to the resume file
+        resumeOriginalName: { type: String }, // Original name of the uploaded resume file
+        company: { type: mongoose.Schema.Types.ObjectId, ref: 'Company' }, // Reference to Company model for recruiters
+        profilePicture: { type: String, default: '' } // URL or path to profile picture
+    }
+}, { timestamps: true });
+
+
+const User = mongoose.model("User", userSchema);
+export default User;
